d7b5a100033185da0485e33ce2de6f4488c3633b
connorobro <connorobro@gmail.com>
2025-04-16 11:26:12 -0700

    Started XML and Java for the Play Minigame

---
+++ app/src/main/java/com/example/tamagotchi/PlayActivity.java
@@ -0,0 +1,16 @@
+package com.yourpackage.name;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MiniGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minigame);
+
+        // TODO: Initialize game logic
+    }
+}
+
