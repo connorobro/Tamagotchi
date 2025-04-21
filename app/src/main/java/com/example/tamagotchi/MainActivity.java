6e50d4f398ea0efe0b04f9bd654f894723dc8d9a
C_Fletes <166450927+Carlos-Fletes@users.noreply.github.com>
2025-04-20 20:43:48 -0700

    Allows the login of the user need to add the logout

--- app/src/main/java/com/example/tamagotchi/MainActivity.java
+++ app/src/main/java/com/example/tamagotchi/MainActivity.java
@@ -1,24 +1,87 @@
 package com.example.tamagotchi;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
+
+import com.example.tamagotchi.database.repository;
+import com.example.tamagotchi.database.entities.User;
+import com.example.tamagotchi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.tamagotchi.USER_ID_KEY";
+    public static final String SHARED_PREFERENCE_FILE_KEY = "com.example.tamagotchi.PREFERENCE_FILE_KEY";
+    public static final String SHARED_PREFERENCE_USERID_KEY = "com.example.tamagotchi.PREFERENCE_USERID_KEY";
+    private static final int LOGGED_OUT = -1;
+
+    private ActivityMainBinding binding;
+    private repository repository;
+    private int loggedInUserId = LOGGED_OUT;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = repository.getRepository(getApplication());
+
+        loginUser(savedInstanceState);
+
+        if (loggedInUserId == LOGGED_OUT) {
+            startActivity(LoginActivity.loginIntentFactory(this));
+            finish();
+            return;
+        }
+
+        updateUI();
+    }
+
+    private void loginUser(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        loggedInUserId = sharedPreferences.getInt(SHARED_PREFERENCE_USERID_KEY, LOGGED_OUT);
+
+        if (loggedInUserId == LOGGED_OUT && savedInstanceState != null) {
+            loggedInUserId = savedInstanceState.getInt(SHARED_PREFERENCE_USERID_KEY, LOGGED_OUT);
+        }
+
+        if (loggedInUserId == LOGGED_OUT) {
+            loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        }
+
+        if (loggedInUserId == LOGGED_OUT) return;
+
+        LiveData<User> userObserver = repository.getUserById(loggedInUserId);
+        userObserver.observe(this, user -> {
+            if (user != null) {
+                this.user = user;
+                binding.titleText.setText("Welcome, " + user.getUsername() + "!");
+            }
         });
+    }
+
+    private void updateUI() {
+        // Placeholder: show welcome message or Tamagotchi state here.
+        binding.titleText.setText("Welcome to Tamagotchi!");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SHARED_PREFERENCE_USERID_KEY, loggedInUserId);
+    }
+
+    public static Intent mainActivityIntentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
     }
 }
--- app/src/main/java/com/example/tamagotchi/PlayActivity.java
+++ app/src/main/java/com/example/tamagotchi/PlayActivity.java
@@ -1,16 +1,17 @@
-package com.yourpackage.name;
+/*
+package com.example.tamagotchi;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MiniGameActivity extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_minigame);
+        setContentView(R.layout.play_minigame);
 
         // TODO: Initialize game logic
     }
 }
-
+*/
