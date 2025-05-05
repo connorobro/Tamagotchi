9c12650204c5cf19d9d63baf06f118c4998d8fc7
Emily <emilycastillogrim@gmail.com>
2025-05-05 11:07:25 -0700

    changed minigame xml to main

--- app/src/main/java/com/example/tamagotchi/minigame/MinigameActivity.java
+++ app/src/main/java/com/example/tamagotchi/minigame/MinigameActivity.java
@@ -8,17 +8,5 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.tamagotchi.R;
 
 public class MinigameActivity extends AppCompatActivity {
-    ImageView imageView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.play_minigame);
-        imageView = findViewById(R.id.imageView2);
 
-    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-
-    }
 }
