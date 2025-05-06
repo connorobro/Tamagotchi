0e666610f3fcb1d0b1e5ae4e0988dc56172c5659
Miguel Oros <moros@csumb.edu>
2025-05-05 20:47:07 -0700

    Did UserTest in the right branch now. Did it in the wrong one before.

--- app/src/test/java/com/example/tamagotchi/database/entities/UserTest.java
+++ app/src/test/java/com/example/tamagotchi/database/entities/UserTest.java
@@ -1,22 +1,26 @@
 package com.example.tamagotchi.database.entities;
 
 import junit.framework.TestCase;
+import static org.junit.Assert.assertNotEquals;
 
 public class UserTest extends TestCase {
 
-    public void testGetId() {
-        //make test
-    }
-
-    public void testGetUsername() {
+    public void setUp() throws Exception {
+        User user1 = new User("admin", "admin", true);
     }
 
     public void testSetUsername() {
+        User user1 = new User("admin", "admin", true);
+        user1.setUsername("realAdmin");
+        assertEquals(user1.getUsername(), "realAdmin");
+        assertNotEquals(user1.getUsername(), "fakeAdmin");
+
     }
 
-    public void testIsAdmin() {
-    }
-
-    public void testSetAdmin() {
+    public void testSetPassword() {
+        User user1 = new User("admin", "admin", true);
+        user1.setPassword("admin");
+        assertEquals(user1.getPassword(), "admin");
+        assertNotEquals(user1.getPassword(), "evilAdmin");
     }
 }
