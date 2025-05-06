77dd7cace6156ae492f9410b9cc6f52da592c143
Miguel Oros <moros@csumb.edu>
2025-05-05 20:39:35 -0700

    finished User unit tests

--- app/src/test/java/com/example/tamagotchi/database/entities/UserTest.java
+++ app/src/test/java/com/example/tamagotchi/database/entities/UserTest.java
@@ -1,26 +1,30 @@
 package com.example.tamagotchi.database.entities;
 
+import static org.junit.Assert.assertNotEquals;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 public class UserTest extends TestCase {
 
+    public void setUp() throws Exception {
+        User user1 = new User("admin", "admin", true );
+    }
+
     @Test
-    public void testGetId() {
-
-
-    }
-
-    public void testGetUsername() {
-    }
-
     public void testSetUsername() {
+        User user1 = new User("admin", "admin", true );
+        user1.setUsername("admin2");
+        assertEquals(user1.getUsername(), "admin2");
+        assertNotEquals(user1.getUsername(), "admin3");
     }
 
-    public void testIsAdmin() {
-    }
-
-    public void testSetAdmin() {
+    @Test
+    public void testSetPassword() {
+        User user1 = new User("admin", "admin", true );
+        user1.setPassword("realAdmin");
+        assertEquals(user1.getPassword(), "realAdmin");
+        assertNotEquals(user1.getPassword(), "evilAdmin");
     }
 }
