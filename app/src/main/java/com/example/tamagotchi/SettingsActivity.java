6c54c61537c63867c567697c1bfcc24de33d48f4
Emily <emilycastillogrim@gmail.com>
2025-05-06 19:57:48 -0700

    fixed color of pet

--- app/src/main/java/com/example/tamagotchi/SettingsActivity.java
+++ app/src/main/java/com/example/tamagotchi/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.tamagotchi;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,26 +22,32 @@ public class SettingsActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView2);
         Button redButton = findViewById(R.id.redButton);
         Button blueButton = findViewById(R.id.blueButton);
+        Button clearButton = findViewById(R.id.clearButton);
         Button saveButton = findViewById(R.id.settingButton);
         redButton.setOnClickListener(view -> {
 
 
                 choice = "red";
-               imageView.setColorFilter(Color.RED);
+               imageView.setColorFilter(Color.RED, PorterDuff.Mode.OVERLAY);
 
 
         });
         blueButton.setOnClickListener(view -> {
 
                 choice = "blue";
-                imageView.setColorFilter(Color.BLUE);
+                imageView.setColorFilter(Color.BLUE, PorterDuff.Mode.OVERLAY);
 
 
         });
+        clearButton.setOnClickListener(view -> {
+            choice = "";
+            imageView.clearColorFilter();
+        });
+
         saveButton.setOnClickListener(view -> {
             SharedPreferences sharedPref = getSharedPreferences("colorsetting", MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putString("petcolor","red");
+            editor.putString("petcolor",choice);
             editor.apply();
             finish();
 
